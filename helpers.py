6243cb848a8ea273fd98e099697448ee0629e5bd
Aaron <aaronli5260@gmail.com>
2024-02-24 02:07:39 -0600

    scrape

--- helpers.py
+++ helpers.py
@@ -1,7 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
 import concurrent.futures
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 
+# Get sitemap from website
 def get_sitemap(url, ignore):
     urls = []
 
@@ -15,11 +21,12 @@ def get_sitemap(url, ignore):
 
         for loc_element in root.findall(".//sitemap:loc", namespaces=ns):
             if loc_element is not None:
-                if 'hotels' in loc_element.text.lower().strip() or ignore == False:
+                if ('best' in loc_element.text.lower().strip() and 'hotels' in loc_element.text.lower().strip()) or ignore == False:
                     urls.append(loc_element.text)
 
     return urls
 
+# Get all relevant links from sitemap
 def crawl_sitemap(sitemap_url):
     urls = get_sitemap(sitemap_url, False)
 
@@ -27,3 +34,67 @@ def crawl_sitemap(sitemap_url):
         links = list(executor.map(lambda url: get_sitemap(url, True), urls))
 
     return [item for sublist in links for item in sublist]
+
+# Function to extract soup from a webpage using Selenium
+def extract_soup_with_selenium(link):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get(link)
+    driver.implicitly_wait(2)
+
+    try:
+        # Wait for the reviews element to be present
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        return soup
+    except TimeoutException:
+        print("Timed out waiting for reviews element.")
+
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        return soup
+    finally:
+        driver.quit()
+    
+def get_hotel_info(link):
+    results = []
+
+    soup = extract_soup_with_selenium(link)
+    container_elements = soup.find_all('div', class_='body__inner-container')
+    if container_elements:
+        for container_element in container_elements:
+            name = ''
+            description = ''
+            amenities = ''
+            address = ''
+            link = ''
+            
+            name_element = container_element.find('div', {'role': 'heading'})
+            if name_element:
+                name = name_element.text.strip()
+            else:
+                name_element = container_element.find('h2')
+                if name_element:
+                    name = name_element.text.strip()
+
+            description_elements = container_element.find_all('p')
+            for description_element in description_elements:
+                if description_element:
+                    description_text = description_element.text.strip()
+                    if '—' in description_text or name in description_text:
+                        description = description_text
+                        # Search for link in description text
+                        outbound_link = description_element.find('a', class_='external-link')
+                        if outbound_link:
+                            link = outbound_link['href']
+                    elif 'Amenities:' in description_text:
+                        amenities = description_text.replace('Amenities:', '').strip()
+                    elif 'Address:' in description_text or 'Location:' in description_text:
+                        address = description_text.replace('Address:', '').strip()
+                        address = description_text.replace('Location:', '').strip()
+            
+            if any([name, link, description, amenities, address]):
+                results.append([name, link, description, amenities, address]) 
+                
+    return results
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from helpers import crawl_sitemap
+from helpers import crawl_sitemap, get_hotel_info
 import pandas as pd
 import concurrent.futures
 
@@ -7,7 +7,14 @@ def scrape_vogue():
     sitemap_url = "https://www.vogue.com/sitemap.xml"
     links = crawl_sitemap(sitemap_url)
 
-    print(links)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        nested_results = list(executor.map(get_hotel_info, links))
     
+    results = [item for sublist in nested_results for item in sublist]
+
+    df = pd.DataFrame(results)
+    df.to_csv('hotel_data.csv', index=False)
+
+
 if __name__ == "__main__":
     scrape_vogue()
